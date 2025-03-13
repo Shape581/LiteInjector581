cdc77f4010f7066ff081744acafba61064ca4212
Shape581 <severusrogue5008@gmail.com>
2025-03-13 22:05:49 +0100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,26 @@
+﻿using Life;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteInjector581
+{
+    public class Main : Plugin
+    {
+        /////////////////////////////////////////////////
+        //Tout le code ne se trouve pas dans ce fichier//
+        /////////////////////////////////////////////////
+
+        public Main(IGameAPI api) : base(api) { }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("[LiteInjector581] - Succes : Initialise !");
+            Console.ResetColor();
+        }
+    }
+}
